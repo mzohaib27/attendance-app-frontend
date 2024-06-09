360e955e9e3db7b6b56e4339040711655c726a98
mzohaib27 <mzohaibhasan27@gmail.com>
2024-06-09 19:55:48 +0400

    update

--- src/Services/ApiCalls.js
+++ src/Services/ApiCalls.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 export const api = axios.create({
   // baseURL: "http://localhost:4500",
-  baseURL: "https://attendance-backend-dd284de1b57d.herokuapp.com/",
+  baseURL: "https://plankton-app-6ipg6.ondigitalocean.app",
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
