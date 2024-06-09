0dcd43cce5de53fdd9b0ee7294a1324595c4f834
mzohaib27 <129533019+mzohaib27@users.noreply.github.com>
2024-06-09 12:56:54 +0400

    Update ApiCalls.js

--- src/Services/ApiCalls.js
+++ src/Services/ApiCalls.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 export const api = axios.create({
-  baseURL: "http://localhost:4500",
+  baseURL:" https://attendance-backend-dd284de1b57d.herokuapp.com/"  ,
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
